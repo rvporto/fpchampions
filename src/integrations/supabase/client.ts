3f2737c084fff451d220bb518e5ec4d522e88a29
rvporto <victorporto98@hotmail.com>
2026-05-07 12:52:51 -0300

    Deploy Vercel

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,7 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js"; 
 
-const SUPABASE_URL = "https://sgbuvlqmbdtfojmwwwyq.supabase.co";
-const SUPABASE_ANON_KEY =
+///Busca das variáveis de ambiente (Vercel) ou usa o fallback (Lovable)
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "https://sgbuvlqmbdtfojmwwwyq.supabase.co";
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || 
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNnYnV2bHFtYmR0Zm9qbXd3d3lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MTM1MzgsImV4cCI6MjA5MzM4OTUzOH0.aoY9YWsaqW1WuV8RHT1SG1zQcv7GOn2IG1sEBdSHv3c";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
@@ -11,3 +12,4 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     autoRefreshToken: true,
   },
 });
+
